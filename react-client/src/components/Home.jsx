816e40fd7b9dd597cf252cdbce7c25037a66553d
Kathy <xu_kathy@outlook.com>
2024-04-14 20:12:36 -0400

    Added logo and edited mainpage

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -1,20 +1,14 @@
+import React from 'react';
+import clothingStoreLogo from './JK.png';
 
-import React, { Component }  from 'react';
-//
-// this is the home page component
-function Home(props)
-{
-
-
+function Home(props) {
     return (
-        <div>
-            <h2> Express - React with CRUD Operations</h2>
-            <p>React front-end calls Express REST API to add, 
-            list, update, or delete a user, create clothing, etc.</p>
+        <div style={{ textAlign: 'center' }}>
+            <img src={clothingStoreLogo} alt="Clothing Store Logo" style={{ width: '200px', height: 'auto' }} />
+            <h2>Clothing Store</h2>
+            <p>React front-end calls Express REST API to add, list, update, or delete a user, create clothing, etc.</p>
         </div>
     );
-
 }
-// withRouter will pass updated match, location, and history props 
-// to the wrapped component whenever it renders.
+
 export default Home;
